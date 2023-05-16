44e5efae4151b309e3cab5a5f893321b70776596
Maxim <uiumba12321@gmail.com>
2023-05-16 23:24:39 +0100

    update

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit3e116f41eb83996f7de7995c9817bcb5
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit3e116f41eb83996f7de7995c9817bcb5', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit3e116f41eb83996f7de7995c9817bcb5', 'loadClassLoader'));
